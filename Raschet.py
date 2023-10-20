e0ccd990496f6b442df383aadefa89988f0ca781
valeev-jpg <marat_valeev_910@mail.ru>
2023-10-20 20:18:00 +0400

    Добавил модуль расчёта.

---
+++ Raschet.py
@@ -0,0 +1,42 @@
+from abc import ABC
+
+
+class Method(ABC):
+
+    def Raschet(self, summa, srok, stavka, procent, everyMonth, dolg):
+        pass
+
+
+class Annuit(Method):
+    def Raschet(self, summa, srok, stavka, procent, everyMonth, dolg):
+        srok = srok.get() * 12
+        everyMonthStavka = stavka.get() / (12 * 100)
+        globalStavka = (1 + everyMonthStavka) ** srok
+        everyMonth1 = int((summa.get() * everyMonthStavka * globalStavka) / (globalStavka - 1))
+        procent1 = int(everyMonth1 * srok - summa.get())
+        dolg2 = int(procent1 + summa.get())
+        procent.set(procent1)
+        everyMonth.set(everyMonth1)
+        dolg.set(dolg2)
+
+
+class Difference(Method):
+    def Raschet(self, summa, srok, stavka, procent, everyMonth, dolg):
+        global procentPart, accuredInterest, ostatok, dolg1, monthlyPayment  # данные для расчетов внутри функции
+        accuredInterest = 0
+        srok = srok.get() * 12  # получаем содержимое поля ввода
+        everyMonthStavka = stavka.get() / (12 * 100)  # ежемесячная ставка
+        everyMonthDolg = summa.get() / srok  # ежемесячнео погашение долга
+        ostatok = summa.get()  # начальный остаток по платежу
+        MaxEveryMonth = ostatok * everyMonthStavka + everyMonthDolg  # маскимальный платеж в первый месяц
+        for i in range(1, srok + 1, 1):  # цикл до последнего месяца платежа
+            procentPart = ostatok * everyMonthStavka
+            monthlyPayment = procentPart + everyMonthDolg
+            ostatok -= everyMonthDolg
+            accuredInterest += procentPart
+        dolg1 = summa.get() + accuredInterest
+        MinEveryMonth = everyMonthDolg
+        procent.set(int(accuredInterest))
+        everyMonth.set(int(MinEveryMonth))
+        dolg.set(int(dolg1))
+
