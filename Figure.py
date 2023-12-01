e501b0b700e0cd9594bf5c9c3aba47db9a39d84c
BakirovDanil <danil.bakiroff84@gmail.com>
2023-12-01 23:22:13 +0300

    Добавил комментарии ко всему, кроме рассчета

--- Figure.py
+++ Figure.py
@@ -2,29 +2,33 @@ from abc import ABC
 from tkinter import ttk
 
 
-class Figure(ABC):
+class Figure(ABC):  # создание класса (в данном случае класс абстрактный)
 
-    def Sozdanie(self, window):
-        pass
-
-
-def on_key_press(event):
-    if event.char.lower() not in ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '\x08']:
-        return "break"
-    elif event.char == '.' in event.widget.get():
-        return "break"
+    def Sozdanie(self, window):  # создание метода класса
+        pass  # заглушка (то есть метод ничего не делает)
 
 
-class Label(Figure):
+def on_key_press(event):  # функция для проверки корректности ввода
+    if event.char.lower() not in ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                                  '\x08']:  # если при нажатии клавиши на клавиатуре, нажатого символа нет в списке, то ввод не производится
+        return "break"  # вернуть break (то есть ничего не делать)
+    elif event.char == '.' in event.widget.get():  # если точка уже бьла введена, то повтороно ввести нельзя будет
+        return "break"  # вернуть break (то есть ничего не делать)
 
-    def Sozdanie(self, window):
-        summaLabel = ttk.Label(text="Сумма кредита:")
-        srokLabel = ttk.Label(text="Срок кредита:")
-        stavkaLabel = ttk.Label(text="Ключевая ставка:")
-        typeLabel = ttk.Label(text="Тип ежемесячных платежей:")
-        EveryMonthLabel = ttk.Label(text="Ежемесячный платеж")
-        ProcentLabel = ttk.Label(text="Начисленные проценты")
-        DolgLabel = ttk.Label(text="Сумма долга")
+
+class Label(Figure):  # создание класса который является наследником от класса Figure
+
+    def Sozdanie(self, window):  # создание функции для создания и размещения объектов типа Label
+        summaLabel = ttk.Label(text="Сумма кредита:")  # создание объекта типа Label с заданным параметром (text)
+        srokLabel = ttk.Label(text="Срок кредита:")  # создание объекта типа Label с заданным параметром (text)
+        stavkaLabel = ttk.Label(text="Ключевая ставка:")  # создание объекта типа Label с заданным параметром (text)
+        typeLabel = ttk.Label(
+            text="Тип ежемесячных платежей:")  # создание объекта типа Label с заданным параметром (text)
+        EveryMonthLabel = ttk.Label(
+            text="Ежемесячный платеж")  # создание объекта типа Label с заданным параметром (text)
+        ProcentLabel = ttk.Label(
+            text="Начисленные проценты")  # создание объекта типа Label с заданным параметром (text)
+        DolgLabel = ttk.Label(text="Сумма долга")  # создание объекта типа Label с заданным параметром (text)
         summaLabel.place(x=25, y=25)  # размещение метки в окне (Сумма кредита)
         srokLabel.place(x=25, y=60)  # размещение метки в окне (Срок кредита)
         stavkaLabel.place(x=25, y=95)  # размещение метки в окне (Ключевая ставка)
@@ -34,8 +38,9 @@ class Label(Figure):
         DolgLabel.place(x=25, y=330)  # размещение метки в окне (Сумма долга)
 
 
-class Entry(Figure):
-    def __init__(self, summa, srok, stavka, EveryMonth, Procent, Dolg):
+class Entry(Figure):  # создание класса который является наследником от класса Figure
+    def __init__(self, summa, srok, stavka, EveryMonth, Procent, Dolg):  # создание конструктора
+        # self - указание на объекты, которые используются внутри класса\метода
         self.summa = summa
         self.srok = srok
         self.stavka = stavka
@@ -43,33 +48,48 @@ class Entry(Figure):
         self.Procent = Procent
         self.Dolg = Dolg
 
-    def Sozdanie(self, window):
-        entr1 = ttk.Entry(width=10, textvariable=self.summa)
-        entr2 = ttk.Entry(width=10, textvariable=self.srok)
-        entr3 = ttk.Entry(width=10, textvariable=self.stavka)
-        entr4 = ttk.Entry(width=10, textvariable=self.EveryMonth, state="readonly")
-        entr5 = ttk.Entry(width=10, textvariable=self.Procent, state="readonly")
-        entr6 = ttk.Entry(width=10, textvariable=self.Dolg, state="readonly")
-        entr1.place(x=185, y=25)
-        entr1.bind("<Key>", on_key_press)
-        entr2.place(x=185, y=60)
-        entr2.bind("<Key>", on_key_press)
-        entr3.place(x=185, y=95)
-        entr3.bind("<Key>", on_key_press)
-        entr4.place(x=200, y=260)
-        entr4.bind("<Key>", on_key_press)
-        entr5.place(x=200, y=295)
-        entr5.bind("<Key>", on_key_press)
-        entr6.place(x=200, y=330)
-        entr6.bind("<Key>", on_key_press)
+    def Sozdanie(self, window):  # создание функции для создания и размещения объектов типа Entry
+        entr1 = ttk.Entry(width=10, textvariable=self.summa)  # создание объекта типа Entry с заданным параметром (
+        # width - ширина, textvariable - с какой переменной он связан)
+        entr2 = ttk.Entry(width=10, textvariable=self.srok)  # создание объекта типа Entry с заданным параметром (
+        # width - ширина, textvariable - с какой переменной он связан)
+        entr3 = ttk.Entry(width=10, textvariable=self.stavka)  # создание объекта типа Entry с заданным параметром (
+        # width - ширина, textvariable - с какой переменной он связан)
+        entr4 = ttk.Entry(width=10, textvariable=self.EveryMonth, state="readonly")  # создание объекта типа Entry с
+        # заданным параметром (width - ширина, textvariable - с какой переменной он связан, state - "readonly",
+        # можно только читать, писать в него нельзя)
+        entr5 = ttk.Entry(width=10, textvariable=self.Procent, state="readonly")  # создание объекта типа Entry с
+        # заданным параметром (width - ширина, textvariable - с какой переменной он связан, state - "readonly",
+        # можно только читать, писать в него нельзя)
+        entr6 = ttk.Entry(width=10, textvariable=self.Dolg, state="readonly")# создание объекта типа Entry с
+        # заданным параметром (width - ширина, textvariable - с какой переменной он связан, state - "readonly",
+        # можно только читать, писать в него нельзя)
+        entr1.place(x=185, y=25)  # размещение объекта в окне
+        entr1.bind("<Key>", on_key_press)  # указание на то, какая функция вызывается при вводе с клавиатуры
+        entr2.place(x=185, y=60)  # размещение объекта в окне
+        entr2.bind("<Key>", on_key_press)  # указание на то, какая функция вызывается при вводе с клавиатуры
+        entr3.place(x=185, y=95)  # размещение объекта в окне
+        entr3.bind("<Key>", on_key_press)  # указание на то, какая функция вызывается при вводе с клавиатуры
+        entr4.place(x=200, y=260)  # размещение объекта в окне
+        entr4.bind("<Key>", on_key_press)  # указание на то, какая функция вызывается при вводе с клавиатуры
+        entr5.place(x=200, y=295)  # размещение объекта в окне
+        entr5.bind("<Key>", on_key_press)  # указание на то, какая функция вызывается при вводе с клавиатуры
+        entr6.place(x=200, y=330)  # размещение объекта в окне
+        entr6.bind("<Key>", on_key_press)  # указание на то, какая функция вызывается при вводе с клавиатуры
 
 
-class RadioButton(Figure):
-    def __init__(self, r_var):
+class RadioButton(Figure): # создание класса который является наследником от класса Figure
+    def __init__(self, r_var): # создание конструктора
+        # self - указание на объекты, которые используются внутри класса\метода
         self.r_var = r_var
 
-    def Sozdanie(self, window):
-        annuit = ttk.Radiobutton(text="Аннуитетные платежи", variable=self.r_var, value=0)
-        differenc = ttk.Radiobutton(text="Дифференцированные платежи", variable=self.r_var, value=1)
-        annuit.place(x=25, y=185)
-        differenc.place(x=25, y=220)
+    def Sozdanie(self, window): # создание функции для создания и размещения объектов типа RadioButton
+        annuit = ttk.Radiobutton(text="Аннуитетные платежи", variable=self.r_var, value=0)  # создание объекта типа
+        # RadioButton с заданным параметром (
+        # text - текст, variable - с какой переменной он связан, value - какое значение ему соответствует)
+        differenc = ttk.Radiobutton(text="Дифференцированные платежи", variable=self.r_var, value=1)  # создание
+        # объекта типа RadioButton с заданным параметром (text - текст, variable - с какой переменной он связан,
+        # value - какое значение ему соответствует)
+        annuit.place(x=25, y=185)  # размещение объекта в окне
+        differenc.place(x=25, y=220)  # размещение объекта в окне
+
--- MainForm.py
+++ MainForm.py
@@ -9,48 +9,48 @@ import Raschet  # импорт
 import Figure
 
 
-def MainForm(window):
+def MainForm(window): # функция для создания формы
     window.title("Ипотечный калькулятор для физических лиц")  # установка названия в окне
     window.iconbitmap()  # установка иконки (добавить позже)
     window.geometry("500x600+400+200")  # размер окна (ширина x высота)
-    window.resizable(False, False)
+    window.resizable(False, False) # запрет на изменение размера окна
     window.protocol("WM_DELETE_WINDOW", finish)  # первое - имя события, второе - вызов функции
-    window.mainloop()
+    window.mainloop() # бесконечное отображение окна приложения
 
 
-def finish():
-    Frame.destroy()
-    print("Закрытие приложения")
+def finish(): # функция для закрытия окна
+    Frame.destroy() # вызывается функция destroy на Frame (графическое окно)
+    print("Закрытие приложения") # вывод надписи в терминал
 
 
 Frame = Tk()  # создание графического окна
 
-summa = IntVar()
-srok = IntVar()
-stavka = DoubleVar()
-EveryMonth = IntVar()
-Procent = IntVar()
-Dolg = IntVar()
+summa = IntVar() # объявление переменной типа IntVar (целочисленное значение)
+srok = IntVar() # объявление переменной типа IntVar (целочисленное значение)
+stavka = DoubleVar() # объявление переменной типа DoubleVar (значение с плавающей запятой)
+EveryMonth = IntVar() # объявление переменной типа IntVar (целочисленное значение)
+Procent = IntVar() # объявление переменной типа IntVar (целочисленное значение)
+Dolg = IntVar() # объявление переменной типа IntVar (целочисленное значение)
 
-r_var = IntVar()
-r_var.set(0)
+r_var = IntVar() # объявление переменной типа IntVar (целочисленное значение). Данная переменная служит для функционирования RadioButton
+r_var.set(0) # установка в переменную значения 0
 
-def Vibor():
-    if r_var.get() == 0:
-        a1 = Raschet.Annuit()
-        a1.Raschet(summa, srok, stavka, Procent, EveryMonth, Dolg)
-    elif r_var.get() == 1:
-        a2 = Raschet.Difference()
-        a2.Raschet(summa, srok, stavka, Procent, EveryMonth, Dolg)
+def Vibor(): # функция (в зависимости от выбранного RadioButton )
+    if r_var.get() == 0: # если значение ноль, то выбирается первый способ расчета
+        a1 = Raschet.Annuit() # создание объекта класса
+        a1.Raschet(summa, srok, stavka, Procent, EveryMonth, Dolg) # использование функции созданного объекта (в скобках передаются параметры, с которыми функция работает)
+    elif r_var.get() == 1: # если значение 1, то выбирается второй способ расчета
+        a2 = Raschet.Difference() # создание объекта класса
+        a2.Raschet(summa, srok, stavka, Procent, EveryMonth, Dolg) # использование функции созданного объекта (в скобках передаются параметры, с которыми функция работает)
 
 
-btn = ttk.Button(text="Рассчитать", command=Vibor)
-btn.place(x=50, y=400)
+btn = ttk.Button(text="Рассчитать", command=Vibor) # создание кнопки на форме (text - это то, что отображается на кнопке, command - какое действие происходит при нажатии на кнопку)
+btn.place(x=50, y=400) # размещение объекта на форме (функция place)
 
-Label = Figure.Label()
-Label.Sozdanie(Frame)
-Entry = Figure.Entry(summa, srok, stavka, EveryMonth, Procent, Dolg)
-Entry.Sozdanie(Frame)
-RadioButton = Figure.RadioButton(r_var)
-RadioButton.Sozdanie(Frame)
-MainForm(Frame)
+Label = Figure.Label() # создание объекта класса
+Label.Sozdanie(Frame) # использование функции созданного объекта (в скобках параметры)
+Entry = Figure.Entry(summa, srok, stavka, EveryMonth, Procent, Dolg) # создание объекта класса (в скобак передаются параметры конструктора)
+Entry.Sozdanie(Frame) # использование функции созданного объекта (в скобках параметры)
+RadioButton = Figure.RadioButton(r_var)#  создание объекта класса (в скобак передаются параметры конструктор
+RadioButton.Sozdanie(Frame) # использование функции созданного объекта (в скобках параметры)
+MainForm(Frame) # использование функции (в скобках передается параметр)
